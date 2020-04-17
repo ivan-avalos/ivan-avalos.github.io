a2caaf3a1acaaa4f314636c2bb2b04e9b2ca4fd6
avalos <ivan.avalos.diaz@hotmail.com>
2020-04-16 23:06:01 -0500

    Added experimental dark mode

--- js/avalos.js
+++ js/avalos.js
@@ -12,3 +12,27 @@ var swiper = new Swiper ('.swiper-container', {
         prevEl: '.swiper-button-prev'
     }
 });
+
+const darkmode = new Darkmode({
+    mixColor: '#000',
+    bottom: '64px'
+});
+
+var setSwitch = function() {
+    var btn = $('#switchTheme');
+    if (darkmode.isActivated()) {
+        btn.html('Light Mode');
+        btn.removeClass('btn-dark').addClass('btn-light').addClass('text-dark');
+    } else {
+        btn.html('Dark Mode');
+        btn.removeClass('btn-light').removeClass('text-dark').addClass('btn-dark');
+    }
+    btn.blur();
+}
+
+var switchTheme = function () {
+    darkmode.toggle();
+    setSwitch();
+};
+
+setSwitch();
