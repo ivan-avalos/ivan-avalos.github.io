fd5b6cc567677d4f2977e397b9a8118ffe174e70
avalos <ivan.avalos.diaz@hotmail.com>
2020-05-24 16:16:03 -0500

    Darkmode.js was unnecessary. YouTube embeds replaced with invidio.us

--- js/avalos.js
+++ js/avalos.js
@@ -1,38 +1,56 @@
-var date = new Date();
-var year = date.getFullYear();
+var globalScheme = localStorage.getItem('scheme');
 
-var swiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
+var swiper = new Swiper('.swiper-container', {
+	direction: 'horizontal',
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination'
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
 });
 
-const darkmode = new Darkmode({
-    mixColor: '#000',
-    bottom: '64px'
-});
-
-var setSwitch = function() {
-    var btn = $('#switchTheme');
-    if (darkmode.isActivated()) {
-        btn.html('Light Mode');
-        btn.removeClass('btn-dark').addClass('btn-light').addClass('text-dark');
-    } else {
-        btn.html('Dark Mode');
-        btn.removeClass('btn-light').removeClass('text-dark').addClass('btn-dark');
-    }
-    btn.blur();
-}
-
-var switchTheme = function () {
-    darkmode.toggle();
-    setSwitch();
+var setScheme = function(scheme) {
+	localStorage.setItem('scheme', scheme);
+	globalScheme = scheme;
+	var body = $('body');
+	var btn = $('#switchTheme');
+	if (scheme == 'dark') {
+		btn.html('Light Mode');
+		btn.removeClass('btn-dark').addClass('btn-light').addClass('text-dark');
+		body.addClass('darkmode--activated');
+	} else if (scheme == 'light') {
+		btn.html('Dark Mode');
+		btn.removeClass('btn-light').removeClass('text-dark').addClass('btn-dark');
+		body.removeClass('darkmode--activated');
+	}
 };
 
-setSwitch();
+var toggleScheme = function() {
+	if (globalScheme == 'dark') {
+		setScheme('light');
+	} else if (globalScheme == 'light') {
+		setScheme('dark');
+	}
+}
+
+var switchTheme = function() {
+	toggleScheme();
+	$('#switchTheme').blur();
+}
+
+var loadScheme = function() {
+	if (globalScheme == null) {
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setScheme('dark');
+		} else {
+			setScheme('light');
+		}
+	} else {
+		setScheme(globalScheme);
+	}
+}
+
+loadScheme();
